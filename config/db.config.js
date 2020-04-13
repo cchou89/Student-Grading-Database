fd513d1d10bd062833b4653988fa87abed33da7a
ckc21 <ckc21@sfu.ca>
2020-04-13 10:09:02 -0700

    Correct config settings

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: "35.227.146.173",
-    USER: "cmpt470",
-    PASSWORD: "readonlyuser",
-    DB: "readonly"
+    USER: "readonlyuser",
+    PASSWORD: "readonly",
+    DB: "cmpt470"
 };
